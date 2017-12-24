c9c6026a722baf872a1fae762135c979aa6c3f98
radrad2 <34811487+radrad2@users.noreply.github.com>
2017-12-24 16:34:37 +0000

    Update CryptoNoteConfig.h
    added genesis block

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -89,7 +89,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Flash_Cash";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff07029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f5b1f41b2f4c5ba62651461deb2667abb668f83a13270baf6a50e4453ab05a09";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
